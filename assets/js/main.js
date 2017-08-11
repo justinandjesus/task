4d3f135899ab67fd4a56763732723fa4adc3cf83
Justin <justinandjesus@gmail.com>
2017-08-11 13:36:28 +0530

    first

---
+++ assets/js/main.js
@@ -0,0 +1,29 @@
+
+$(document).ready(function(){
+  
+    
+// Add smooth scrolling to all links
+  $("a.smoothClick").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+
+    
+    
+  
+});
